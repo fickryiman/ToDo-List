a939ffe02376da4e66418b8461e0eef97610d609
Fickry Iman <fickry.iman@gmail.com>
2023-03-30 23:37:42 +0700

    finished project 3: interactive list
    - Add a new JavaScript file and import it as a module
    - Add event listener to the checkbox (change).
    - Update items object's value for completed key upon user actions.
    - Implement a function for the "Clear all completed" button (use filter() method).
    - Store the updated array of items in local storage, so the user gets the correct list of values after the page reloads (which means that any changes made to the list should be preserved).

--- src/index.js
+++ src/index.js
@@ -1,87 +1,7 @@
 import './style.css';
-
-const Todo = (index, description, completed) => ({
-  index, description, completed,
-});
-
-const TodoCRUD = {
-  readTodos: () => {
-    const tasks = localStorage.getItem('TODOS') === null ? [] : JSON.parse(localStorage.getItem('TODOS'));
-    return tasks;
-  },
-  createTodo: (todo) => {
-    const tasks = TodoCRUD.readTodos();
-    tasks.push(todo);
-    localStorage.setItem('TODOS', JSON.stringify(tasks));
-  },
-  updateTodoCompletion: (index) => {
-    let tasks = TodoCRUD.readTodos();
-
-    tasks = tasks.map((task, i) => {
-      const temporaryStorage = {};
-      temporaryStorage.index = task.index;
-      temporaryStorage.description = task.description;
-      temporaryStorage.completed = (i === index - 1) ? !task.completed : task.completed;
-      return temporaryStorage;
-    });
-    localStorage.setItem('TODOS', JSON.stringify(tasks));
-  },
-  updateTodoDescription: (index, event) => {
-    let tasks = TodoCRUD.readTodos();
-
-    tasks = tasks.map((task, i) => {
-      const temporaryStorage = {};
-      temporaryStorage.index = task.index;
-      temporaryStorage.description = (i === index - 1) ? event.target.value : task.description;
-      temporaryStorage.completed = task.completed;
-      return temporaryStorage;
-    });
-    localStorage.setItem('TODOS', JSON.stringify(tasks));
-  },
-  deleteTodo: (index) => {
-    let tasks = TodoCRUD.readTodos();
-    tasks = tasks.filter((todo, ref) => ref !== +index);
-    tasks = tasks.map((task, index) => ({ ...task, index: index + 1 }));
-
-    localStorage.setItem('TODOS', JSON.stringify(tasks));
-  },
-  deleteCompletedTodo: () => {
-    let tasks = TodoCRUD.readTodos();
-    tasks = tasks.filter((todo) => !todo.completed);
-
-    localStorage.setItem('TODOS', JSON.stringify(tasks));
-  },
-};
-
-const Interface = {
-  renderTodos: () => {
-    const todoListsContainer = document.querySelector('.container-todo-lists');
-    const tasks = TodoCRUD.readTodos();
-    todoListsContainer.innerHTML = tasks.map((todo, index) => ((todo.completed) ? ` 
-                            <div class="wrap-todo-list"">
-                              <div class="todo-list"">
-                                <input type="checkbox" class="todo-checkbox" id="todo-${index + 1}" name="todo-${index + 1}" checked>
-                                <input id="todo-desc-${index + 1}" class="todo-description-false" desc-index="${index + 1}" value="${todo.description}"></input>
-                                <a class="a-todo-options">
-                                  <img src="../icons/dots1.png" class="todo-options" data-index="${index}">
-                                </a>
-                              </div>
-                              <hr>
-                            </div>
-                          ` : `
-                            <div class="wrap-todo-list"">
-                              <div class="todo-list"">
-                                <input type="checkbox" class="todo-checkbox" id="todo-${index + 1}" name="todo-${index + 1}">
-                                <input id="todo-desc-${index + 1}" class="todo-description" desc-index="${index + 1}" value="${todo.description}"></input>
-                                <a class="a-todo-options">
-                                  <img src="../icons/dots1.png" class="todo-options" data-index="${index}">
-                                </a>
-                              </div>
-                              <hr>
-                            </div>
-                          `)).join('');
-  },
-};
+import TodoCRUD from '../modules/crud.js';
+import Interface from '../modules/interface.js';
+import Todo from '../modules/todo.js';
 
 document.addEventListener('DOMContentLoaded', Interface.renderTodos);
 
@@ -112,7 +32,7 @@ todoListsContainer.addEventListener('click', (event) => {
       event.target.nextElementSibling.classList = 'todo-description-false';
     } else {
       event.target.nextElementSibling.classList = 'todo-description';
-    }
+    } 
     TodoCRUD.updateTodoCompletion(event.target.id.slice(-1));
     Interface.renderTodos();
   }
