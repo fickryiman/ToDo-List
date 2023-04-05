699646dd6e91771e9a45e4d62f4dd8fc12b4afa3
Fickry Iman <fickry.iman@gmail.com>
2023-04-06 00:04:03 +0700

    create add todo unit testing

---
+++ modules/test/addDeleteTodo.js
@@ -0,0 +1,28 @@
+let todos = [];
+
+const TodoCRUD = {
+
+  readTodos: () => {
+    return todos;
+  },
+  createTodo: (desc) => {
+    todos.push({
+      index: todos.length + 1,
+      description: desc,
+      isCompleted: false,
+    });
+  },
+  deleteTodo: (index) => {
+    todos = todos.filter((todo, ref) => ref !== +index);
+    todos = todos.map((todo, index) => ({ ...todo, index: index + 1 }));
+    return todos;
+  },
+  resetTodo: () => {
+    todos = [];
+    return todos;
+  }
+};
+
+module.exports = { todos, TodoCRUD };
+
+TodoCRUD.resetTodo();
--- modules/test/addDeleteTodo.test.js
+++ modules/test/addDeleteTodo.test.js
@@ -1,23 +1,18 @@
-const TodoCRUD = require('../crud');
+const { todos, TodoCRUD } = require('./addDeleteTodo');
 
-// unit test for add function
-describe('add new item to todo list', () => {
-  const Todos = [
-    {"index":1, "description":"setup project with webpack", "completed":false},
-    {"index":2, "description":"setup linter and github flow", "completed":false},
-    {"index":3, "description":"project 1: list structure", "completed":false},
-    {"index":4, "description":"project 2: add and remove items", "completed":false},
-    {"index":5, "description":"project 3: interactive list", "completed":false},
-    {"index":6, "description":"freelancer projects", "completed":false},
-    {"index":7, "description":"finish test part-1", "completed":false}
-  ];
-
-  test('check if add Todo function is exists', () => {
+describe('test add todo list', () => {
+  test('createTodo function is exists?', () => {
     expect(TodoCRUD.createTodo).toBeDefined();
   });
-
-  test('check if storage is the same as the value added', () => {
-    expect(TodoCRUD.createTodo('setup project with webpack').toEqual(Todos[6].description));
+  test('add item to todo list check the description', () => {
+    expect(TodoCRUD.createTodo('finish exercise test part-1')).toEqual(todos.description);
   });
-
+  test('add item to todo list check the index', () => {
+    expect(TodoCRUD.createTodo('finish exercise test part-2')).toEqual(todos.index);
+  });
+  test('add item to todo list check isCompleted', () => {
+    expect(TodoCRUD.createTodo('finish exercise test part-3')).toEqual(todos.isCompleted);
+  });
 });
+
+
